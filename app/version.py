e499414609db347a01f27687d08ec98335c8abbb
Campbell Brown <campbellbrown093@gmail.com>
2024-11-29 10:56:14 +1300

    Missing GIT_SHA in version.py

--- app/version.py
+++ app/version.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 
 version_path = "VERSION"
@@ -6,3 +7,5 @@ if hasattr(sys, "_MEIPASS"):
     version_path = os.path.join(sys._MEIPASS, version_path)
 with open(version_path) as version_file:
     __version__ = version_file.read().strip()
+
+GIT_SHA = subprocess.check_output(["git", "rev-parse", "--short=8", "HEAD"]).decode("utf-8").strip()
