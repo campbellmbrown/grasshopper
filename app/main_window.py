f032cd6550100b3ed9cc8bb4a74bdd6016d0f2b7
Campbell Brown <campbellbrown093@gmail.com>
2024-11-28 17:59:37 +1300

    Added logging dock

--- app/main_window.py
+++ app/main_window.py
@@ -1,4 +1,17 @@
-from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget
+import logging
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QDockWidget, QMainWindow, QTextEdit, QVBoxLayout, QWidget
+
+
+class Logger(logging.Handler):
+    def __init__(self, widget: QTextEdit):
+        super().__init__()
+        self.widget = widget
+
+    def emit(self, record):
+        msg = self.format(record)
+        self.widget.append(msg)
 
 
 class MainWindow(QMainWindow):
@@ -9,6 +22,25 @@ class MainWindow(QMainWindow):
 
         layout = QVBoxLayout()
 
+        dock = QDockWidget("Log")
+        log_text = QTextEdit()
+        log_text.setReadOnly(True)
+        log_text.setLineWrapMode(QTextEdit.LineWrapMode.NoWrap)
+        # Use a fixed width font
+        log_text.setFontFamily("Consolas")
+        log_handler = Logger(log_text)
+        dock.setWidget(log_handler.widget)
+        dock.setFeatures(
+            QDockWidget.DockWidgetFeature.DockWidgetMovable | QDockWidget.DockWidgetFeature.DockWidgetFloatable
+        )
+
+        log_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+        logging.getLogger().addHandler(log_handler)
+        logging.getLogger().setLevel(logging.INFO)
+
+        logging.info("Application started")
+
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
+        self.addDockWidget(Qt.DockWidgetArea.BottomDockWidgetArea, dock)
