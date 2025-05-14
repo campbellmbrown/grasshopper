b7f5662d50be965f340e1e3f65f4164610ccb9ff
Campbell Brown <campbellbrown093@gmail.com>
2025-05-14 12:24:07 +1200

    Exception popup

---
+++ app/dialogs/exception_view.py
@@ -0,0 +1,33 @@
+import sys
+import traceback
+from types import TracebackType
+
+from PyQt6.QtWidgets import QApplication, QMessageBox
+
+from app.utility.resource_provider import get_icon
+
+
+class ExceptionView(QMessageBox):
+    def __init__(self, exc_type: type[BaseException], exc_value: BaseException, exc_tb: TracebackType | None) -> None:
+        super().__init__()
+        self.setIcon(QMessageBox.Icon.Critical)
+        try:
+            self.setWindowIcon(get_icon("logo_32x32.png"))
+        except Exception:
+            pass
+        self.setWindowTitle("Unhandled Exception")
+        self.setText(f"An unhandled exception occurred: {exc_value}")
+
+        exception_details = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+        self.setDetailedText(exception_details)
+
+    @staticmethod
+    def show_exception_dialog(
+        exc_type: type[BaseException], exc_value: BaseException, exc_tb: TracebackType | None
+    ) -> None:
+        # Let the exception propagate to the default handler so code editors can show the traceback.
+        sys.__excepthook__(exc_type, exc_value, exc_tb)
+        dialog = ExceptionView(exc_type, exc_value, exc_tb)
+        dialog.raise_()
+        dialog.exec()
+        QApplication.exit(1)
--- grasshopper.py
+++ grasshopper.py
@@ -2,11 +2,13 @@ import sys
 
 from PyQt6.QtWidgets import QApplication
 
+from app.dialogs.exception_view import ExceptionView
 from app.main.main_controller import MainController
 from app.main.main_view import MainView
 from app.model.model import Model
 
 if __name__ == "__main__":
+    sys.excepthook = ExceptionView.show_exception_dialog
     app = QApplication(sys.argv)
 
     model = Model()
@@ -14,4 +16,4 @@ if __name__ == "__main__":
     main_controller = MainController(main_view, model)
 
     main_view.show()
-    app.exec()
+    sys.exit(app.exec())
